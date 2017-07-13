9bb52c5605be39196cc4dc2210d74d7aee550960
Sam Salefske <Sam.salefske@gmail.com>
2017-07-12 22:31:57 -0400

    Initial Commit

---
+++ RoboHash Generator/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace RoboHash_Generator
+{
+    partial class RobotImageGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rbRobot1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbRobot2 = new System.Windows.Forms.RadioButton();
+            this.rbRobot3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbBackground1 = new System.Windows.Forms.RadioButton();
+            this.rbBackground2 = new System.Windows.Forms.RadioButton();
+            this.rbBackground3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(318, 264);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 48);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(225, 377);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10);
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rbRobot1
+            // 
+            this.rbRobot1.AutoSize = true;
+            this.rbRobot1.Location = new System.Drawing.Point(34, 21);
+            this.rbRobot1.Name = "rbRobot1";
+            this.rbRobot1.Size = new System.Drawing.Size(62, 21);
+            this.rbRobot1.TabIndex = 3;
+            this.rbRobot1.TabStop = true;
+            this.rbRobot1.Text = "Set 1";
+            this.rbRobot1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbRobot3);
+            this.groupBox1.Controls.Add(this.rbRobot2);
+            this.groupBox1.Controls.Add(this.rbRobot1);
+            this.groupBox1.Location = new System.Drawing.Point(318, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 117);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Robot Set";
+            // 
+            // rbRobot2
+            // 
+            this.rbRobot2.AutoSize = true;
+            this.rbRobot2.Location = new System.Drawing.Point(34, 48);
+            this.rbRobot2.Name = "rbRobot2";
+            this.rbRobot2.Size = new System.Drawing.Size(62, 21);
+            this.rbRobot2.TabIndex = 4;
+            this.rbRobot2.TabStop = true;
+            this.rbRobot2.Text = "Set 2";
+            this.rbRobot2.UseVisualStyleBackColor = true;
+            // 
+            // rbRobot3
+            // 
+            this.rbRobot3.AutoSize = true;
+            this.rbRobot3.Location = new System.Drawing.Point(34, 76);
+            this.rbRobot3.Name = "rbRobot3";
+            this.rbRobot3.Size = new System.Drawing.Size(62, 21);
+            this.rbRobot3.TabIndex = 5;
+            this.rbRobot3.TabStop = true;
+            this.rbRobot3.Text = "Set 3";
+            this.rbRobot3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbBackground3);
+            this.groupBox2.Controls.Add(this.rbBackground2);
+            this.groupBox2.Controls.Add(this.rbBackground1);
+            this.groupBox2.Location = new System.Drawing.Point(318, 141);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(185, 117);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Background";
+            // 
+            // rbBackground1
+            // 
+            this.rbBackground1.AutoSize = true;
+            this.rbBackground1.Location = new System.Drawing.Point(34, 27);
+            this.rbBackground1.Name = "rbBackground1";
+            this.rbBackground1.Size = new System.Drawing.Size(117, 21);
+            this.rbBackground1.TabIndex = 0;
+            this.rbBackground1.TabStop = true;
+            this.rbBackground1.Text = "Background 1";
+            this.rbBackground1.UseVisualStyleBackColor = true;
+            // 
+            // rbBackground2
+            // 
+            this.rbBackground2.AutoSize = true;
+            this.rbBackground2.Location = new System.Drawing.Point(34, 55);
+            this.rbBackground2.Name = "rbBackground2";
+            this.rbBackground2.Size = new System.Drawing.Size(117, 21);
+            this.rbBackground2.TabIndex = 1;
+            this.rbBackground2.TabStop = true;
+            this.rbBackground2.Text = "Background 2";
+            this.rbBackground2.UseVisualStyleBackColor = true;
+            // 
+            // rbBackground3
+            // 
+            this.rbBackground3.AutoSize = true;
+            this.rbBackground3.Location = new System.Drawing.Point(34, 82);
+            this.rbBackground3.Name = "rbBackground3";
+            this.rbBackground3.Size = new System.Drawing.Size(127, 21);
+            this.rbBackground3.TabIndex = 2;
+            this.rbBackground3.TabStop = true;
+            this.rbBackground3.Text = "No Background";
+            this.rbBackground3.UseVisualStyleBackColor = true;
+            // 
+            // RobotImageGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "RobotImageGenerator";
+            this.Text = "Robot Image Generator";
+            this.Load += new System.EventHandler(this.RobotImageGenerator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rbRobot1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbRobot3;
+        private System.Windows.Forms.RadioButton rbRobot2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbBackground2;
+        private System.Windows.Forms.RadioButton rbBackground1;
+        private System.Windows.Forms.RadioButton rbBackground3;
+    }
+}
+
---
+++ RoboHash Generator/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace RoboHash_Generator
+{
+    public partial class RobotImageGenerator : Form
+    {
+        public RobotImageGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync("https://robohash.org/"+textBox1.Text))
+                {
+                    if (textBox1.Text != null || textBox1.Text != "")
+                    {
+                        textBox1.Text = Dns.GetHostName().ToString();
+                    }
+                    if (rbBackground1.Checked)
+                    {
+                        if (rbRobot1.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set1&size=250x250&bgset=bg1");
+                        if (rbRobot2.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set2&size=250x250&bgset=bg1");
+                        if (rbRobot3.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set3&size=250x250&bgset=bg1");
+                    }
+                    else if (rbBackground2.Checked)
+                    {
+                        if (rbRobot1.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set1&size=250x250&bgset=bg2");
+                        if (rbRobot2.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set2&size=250x250&bgset=bg2");
+                        if (rbRobot3.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set3&size=250x250&bgset=bg2");
+                    }
+                    else if (rbBackground3.Checked)
+                    {
+                        if (rbRobot1.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set1&size=250x250");
+                        if (rbRobot2.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set2&size=250x250");
+                        if (rbRobot3.Checked)
+                            pictureBox1.Load("https://robohash.org/" + textBox1.Text + "?set=set3&size=250x250");
+                    }
+                }
+            }
+        }
+
+        private void RobotImageGenerator_Load(object sender, EventArgs e)
+        {
+            rbRobot1.Checked = true; rbBackground3.Checked = true;
+            textBox1.Text = Dns.GetHostName();
+        }
+    }
+}
